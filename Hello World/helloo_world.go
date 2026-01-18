dceb5059290982e0e3c27d38283ab9faea54f03f
Vladyslav Davydenko <5vladik068@gmail.com>
2026-01-18 12:29:32 +0200

    initial commit

---
+++ Hello World/helloo_world.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Print("test new line ")
+	fmt.Print("we are using new line in the code but should be the line in the terminal")
+}
