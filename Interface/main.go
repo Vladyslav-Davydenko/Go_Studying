d409bf7f226b958beed5e3eb0481382d93222363
Vladyslav Davydenko <5vladik068@gmail.com>
2026-01-26 20:39:01 +0200

    8

---
+++ Interface/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	height, width float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.height * r.width
+}
+
+func (r rect) perim() float64 {
+	return 2 * r.width + 2 * r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius 
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func detectCircle(g geometry){
+	if c, ok := g.(circle); ok {
+		fmt.Println(c, ok)
+	}
+}
+
+func main(){
+  r := rect{10, 20}
+  c := circle{5}
+
+  measure(r)
+  measure(c)
+
+  detectCircle(r)
+  detectCircle(c)
+}
